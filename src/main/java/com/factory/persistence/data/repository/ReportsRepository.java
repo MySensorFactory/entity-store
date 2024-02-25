e2e108309a44cd9956eb63857112993e49d5be4f
damiano <macmacroll@gmail.com>
2024-02-25 22:49:29 +0100

    Changed ReportsRepository to JpaRepository

--- src/main/java/com/factory/persistence/data/repository/ReportsRepository.java
+++ src/main/java/com/factory/persistence/data/repository/ReportsRepository.java
@@ -1,8 +1,8 @@
 package com.factory.persistence.data.repository;
 
 import com.factory.persistence.data.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.ZonedDateTime;
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 
 @Repository
-public interface ReportsRepository extends CrudRepository<Report, UUID> {
+public interface ReportsRepository extends JpaRepository<Report, UUID> {
 
     @Query("SELECT r FROM Report r WHERE :from < r.from AND :to > r.to")
     List<Report> findAllByTimeWindow(ZonedDateTime from, ZonedDateTime to);
